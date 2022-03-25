c4149de578bc0e136d20bfc21aaf27af92c063bd
anthony luque <kasuru@hotmail.com>
2022-03-24 20:58:39 -0500

    [ADD] class to see more clean

--- core/ink/main.h
+++ core/ink/main.h
@@ -1,3 +1,12 @@
 #pragma once
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#define pdSECOND pdMS_TO_TICKS(1000)
+
+class Main final
+{
+public:
+  esp_err_t setup(void);
+  void loop(void);
+};
--- core/src/main.cpp
+++ core/src/main.cpp
@@ -3,11 +3,29 @@
 #define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
 #define LOG_TAG "MAIN"
 
+static Main my_main;
+
 extern "C" void app_main(void)
 {
+    ESP_ERROR_CHECK(my_main.setup());
+
     while (true)
     {
-        ESP_LOGI(LOG_TAG, "Hola Juy con un pipio en el brazo\n");
-        vTaskDelay(2000);
+        my_main.loop();
     }
+}
+
+esp_err_t Main::setup(void)
+{
+    esp_err_t status{ESP_OK};
+
+    ESP_LOGI(LOG_TAG, "Hola!!");
+
+    return status;
+}
+
+void Main::loop(void)
+{
+    ESP_LOGI(LOG_TAG, "Hola Juy con un pipio en el brazo\n");
+    vTaskDelay(pdSECOND);
 }
